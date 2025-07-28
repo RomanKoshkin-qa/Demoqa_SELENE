e2e58fc8c4361ecf52bc41044e50e88b57f748be
RomanKoshkin-qa <oksana.oksana.oksana9@yandex.ru>
2025-07-28 11:47:20 +0300

    Поправил форматирование кода

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,8 +1,5 @@
-import pytest
-from selene import browser, have, be
+from selene import browser, be
 from pathlib import Path
-
-from selenium.webdriver import Keys
 
 
 def test_tasks_demoqa():
@@ -13,31 +10,30 @@ def test_tasks_demoqa():
     browser.element('label[for="gender-radio-1"]').click()
     browser.element('#userNumber').type('9031234567')
     # # работа с  календарем
-    browser.element('#dateOfBirthInput').click() # открыли календарь
+    browser.element('#dateOfBirthInput').click()  # открыли календарь
     # #выбрали год
-    browser.element('.react-datepicker__year-select').click() # Кликнуть по селекту года
+    browser.element('.react-datepicker__year-select').click()  # Кликнуть по селекту года
     option_element = browser.element('.react-datepicker__year-select option[value="2007"]')
     option_element.wait_until(be.present)
     option_element.click()
     # #выбираем месяц
     browser.element('.react-datepicker__month-select').click()  # Кликнуть по селекту года
-    option_element= browser.element('.react-datepicker__month-select option[value="2"]') #март
+    option_element = browser.element('.react-datepicker__month-select option[value="2"]')  # март
     option_element.wait_until(be.present)
     option_element.click()
     # #выбор дня
-    browser.element('.react-datepicker__day--004').click() #4 марта
-    browser.element('#subjectsInput').type("maths").press_enter() #Выбор Maths
-    #клик по hobbies
+    browser.element('.react-datepicker__day--004').click()  # 4 марта
+    browser.element('#subjectsInput').type("maths").press_enter()  # Выбор Maths
+    # клик по hobbies
     hobby_label = browser.element('label[for="hobbies-checkbox-1"]')
     browser.execute_script("arguments[0].click();", hobby_label.locate())
-    #загрузка файла
-    file_path = Path(__file__).parent /  "ones.png"  # Укажите правильный путь
+    # загрузка файла
+    file_path = Path(__file__).parent / "ones.png"  # Укажите правильный путь
     browser.element('#uploadPicture').send_keys(str(file_path))
-    #адрес
+    # адрес
     browser.element('#currentAddress').type('hello')
     browser.element("#react-select-3-input").type("NCR").press_enter()
     browser.element("#react-select-4-input").type("Delhi").press_enter()
     browser.element("#submit").click()
-
 
     browser.quit()
