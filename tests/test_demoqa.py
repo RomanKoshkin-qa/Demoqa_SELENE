b437d6bf7310822d233cac4b5c0815588a3a2a80
RomanKoshkin-qa <oksana.oksana.oksana9@yandex.ru>
2025-07-30 21:09:00 +0300

    Changed code after yashaka comments

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,39 +1,47 @@
-from selene import browser, be
+from selene import browser, be, have
 from pathlib import Path
 
 
 def test_tasks_demoqa():
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.element('#firstName').type('Роман')
-    browser.element('#lastName').type('Кошкин')
-    browser.element('#userEmail').type('Roman@ya.ru')
-    browser.element('label[for="gender-radio-1"]').click()
+
+    # WHEN
+    browser.element('#firstName').type('Roman')
+    browser.element('#lastName').type('Koshkin')
+    browser.element('#userEmail').type('Roman@gmail.com')
+    browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()  #.. это вверх подняться
     browser.element('#userNumber').type('9031234567')
-    # # работа с  календарем
     browser.element('#dateOfBirthInput').click()  # открыли календарь
-    # #выбрали год
-    browser.element('.react-datepicker__year-select').click()  # Кликнуть по селекту года
-    option_element = browser.element('.react-datepicker__year-select option[value="2007"]')
-    option_element.wait_until(be.present)
-    option_element.click()
-    # #выбираем месяц
-    browser.element('.react-datepicker__month-select').click()  # Кликнуть по селекту года
-    option_element = browser.element('.react-datepicker__month-select option[value="2"]')  # март
-    option_element.wait_until(be.present)
-    option_element.click()
-    # #выбор дня
-    browser.element('.react-datepicker__day--004').click()  # 4 марта
-    browser.element('#subjectsInput').type("maths").press_enter()  # Выбор Maths
-    # клик по hobbies
-    hobby_label = browser.element('label[for="hobbies-checkbox-1"]')
+    browser.element('.react-datepicker__month-select').type('May')
+    browser.element('.react-datepicker__year-select').type('1999')
+    browser.element(f'.react-datepicker__day--0{11}').click()
+    browser.element('#subjectsInput').type("Maths").press_enter()  # Subjects
+    hobby_label = browser.element('label[for="hobbies-checkbox-1"]') #Hobbies
     browser.execute_script("arguments[0].click();", hobby_label.locate())
-    # загрузка файла
-    file_path = Path(__file__).parent / "ones.png"  # Укажите правильный путь
+    file_path = Path(__file__).parent / "ones.png"
     browser.element('#uploadPicture').send_keys(str(file_path))
-    # адрес
-    browser.element('#currentAddress').type('hello')
-    browser.element("#react-select-3-input").type("NCR").press_enter()
-    browser.element("#react-select-4-input").type("Delhi").press_enter()
+    browser.element('#currentAddress').type('Moscow Guena Street')
+    browser.element('#state').click()
+    # ^id начинается с react-select и внутри есть option, выбираем из них по тексту(exact)
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('Haryana')).click()
+    browser.element('#city').click()
+    browser.all('[id^=react-select][id*=option]').element_by(have.exact_text('Karnal')).click()
     browser.element("#submit").click()
+
+    # THEN
+    browser.element('.table').all('td').should(
+        have.texts(
+            ('Student Name', 'Roman Koshkin'),
+            ('Student Email', 'Roman@gmail.com'),
+            ('Gender', 'Male'),
+            ('Mobile', '9031234567'),
+            ('Date of Birth', '11 May,1999'),
+            ('Subjects', 'Maths'),
+            ('Hobbies', 'Sports'),
+            ('Picture', 'ones.png'),
+            ('Address', 'Moscow Guena Street'),
+            ('State and City', 'Haryana Karnal'),
+        )
+    )
 
     browser.quit()
